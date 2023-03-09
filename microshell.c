687f139a36f99722e2fa903b5bc5934ad87b501a
med-ghali <62386753+med-ghali@users.noreply.github.com>
2023-03-09 10:43:52 +0100

    no leak

--- microshell.c
+++ microshell.c
@@ -1,223 +1,246 @@
-#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
-void syscall_error();
-typedef struct cmds {
+typedef struct commands{
 	char **opt;
-	struct cmds *next;
-} cmds;
+	struct commands *next;
+}commands;
 
 int ft_strlen(char *s)
 {
-	int i =0;
+	int i = 0;
 	while(s[i])
 		i++;
-	return (i);
+	return i;
 }
 
-void ft_putstr(char *s,int fd)
+void ft_putstr(char *s)
 {
-	int check = write(fd,s,ft_strlen(s));
-	if (check == -1)
-		syscall_error();
+	write(2,s,ft_strlen(s));
+}
+
+void cd_bad_args()
+{
+	ft_putstr("error: cd: bad arguments\n");
+}
+
+void cd_bad_path(char *path)
+{
+	ft_putstr("error: cd: cannot change directory to ");
+	ft_putstr(path);
+	ft_putstr("\n");
 }
 
 void syscall_error()
 {
-	ft_putstr("error: fatal\n",2);
+	ft_putstr("error: fatal\n");
 	exit(1);
 }
 
 void execve_error(char *cmd)
 {
-	ft_putstr("error: cannot execute ",2);
-	ft_putstr(cmd,2);
-	ft_putstr("\n",2);
+	ft_putstr("error: cannot execute ");
+	ft_putstr(cmd);
+	ft_putstr("\n");
 	exit(1);
-}
-
-void cd_wrong_argument()
-{
-	ft_putstr("error: cd: bad arguments\n",2);
-}
-
-void cd_failed(char *path)
-{
-	ft_putstr("error: cd: cannot change directory to ",2);
-	ft_putstr(path,2);
-	ft_putstr("\n",2);
 }
 
 char **next_cmd(char **av)
 {
 	int f = 0;
-
-	while(*av)
+	while (*av)
 	{
-		while (*av && !strcmp(*av,";"))
+		while(*av && !strcmp(*av,";"))
 		{
-			*av = NULL;
+			f =1;
+			*av = 0;
 			av ++;
-			f = 1;
 		}
 		if (f)
 			return av;
 		av++;
 	}
-	return (av);
+	return av;
+	
 }
-
-cmds *new_node(char **av)
+commands *new_node(char **av)
 {
-	cmds *new = malloc(sizeof(cmds));
+	commands *new = malloc(sizeof(commands));
 	if (!new)
 		syscall_error();
 	new->opt = av;
 	new->next = 0;
 	return new;
 }
-
-void add_back(cmds *cmd,char **av)
+void add_back(commands *cmd, char **av)
 {
-	cmds *new = new_node(av);
 	while(cmd->next)
 		cmd = cmd->next;
-	cmd->next = new;
+	cmd->next = new_node(av);
 }
 
-char **parse(cmds **cmd,char **av)
+char **parse(commands **cmds,char **av)
 {
-	cmds *new = 0 ;
-	while(*av &&!strcmp(*av,";"))
+	commands * cmd = 0;
+	while(*av && !strcmp(*av,";"))
 		av++;
-	if (*av == NULL)
+	if (!(*av))
+	{
+		*cmds = 0;
 		return NULL;
+	}
 	char **next = next_cmd(av);
-	new = new_node(av);
+	cmd = new_node(av);
 	while(*av)
 	{
 		if (!strcmp(*av,"|"))
 		{
-			*av = NULL;
-			av++;
-			add_back(new,av);
+			*av = 0;
+			av ++;
+			add_back(cmd,av);
 		}
 		else
 			av++;
 	}
-	*cmd = new;
+	*cmds = cmd;
 	return next;
 }
 
-void print_list(cmds *cmd)
+
+void ft_chdir(commands *cmd)
 {
-	while(cmd)
+	if (!cmd->opt[1])
 	{
-		int i = 0;
-		printf("next node : ------------- \n");
-		while(cmd->opt[i])
+		cd_bad_args();
+		return ;
+	}
+	if (chdir(cmd->opt[1]) == -1 )
+		cd_bad_path(cmd->opt[1]);
+}
+
+void exec_single_cmd(commands *cmds, char **envp)
+{
+	int pid;
+	pid = fork();
+	if (pid == -1)
+		syscall_error();
+	if (pid ==0 )
+	{
+		if (!strcmp(cmds->opt[0],"cd"))
 		{
-			printf("%s\n",cmd->opt[i]);
-			i++;
+			ft_chdir(cmds);
+			exit(1);
 		}
-		cmd = cmd->next;
+		if (execve(cmds->opt[0],cmds->opt,envp) == -1)
+			execve_error(cmds->opt[0]);
+		exit(1);
 	}
 }
 
-void ft_chdir(cmds *cmd)
-{
-	int ret;
-	if (cmd->opt[1])
-	{
-		ret =chdir(cmd->opt[1]);
-		if (ret == -1)
-			cd_failed(cmd->opt[1]);
-	}
-	else
-		cd_wrong_argument();
-}
-
-void exec_single_cmd(cmds *cmd,char **envp)
-{
-	if(!strcmp(cmd->opt[0],"cd"))
-		ft_chdir(cmd);
-	else
-	{
-		int pid = fork();
-		if (pid == 0)
-		{
-			if (execve(cmd->opt[0],cmd->opt,envp) == -1)
-				execve_error(cmd->opt[0]);
-		}
-		else
-			return ;
-	}
-}
-
-void exec_cmds(cmds *cmd,char **envp)
+void execute(commands *cmds, char **envp)
 {
 	int tmp_in = dup(0);
-	int fds[2];
 	int pid;
-
-	while(cmd)
+	if (tmp_in == -1)
+		syscall_error();
+	int fds[2];
+	while(cmds)
 	{
-		if (cmd->next)
-			pipe(fds);
-		if (!cmd->next)
-			exec_single_cmd(cmd,envp);
+		if (!cmds->next)
+			exec_single_cmd(cmds,envp);
 		else
 		{
+			if (pipe(fds) == -1)
+				syscall_error();
 			pid = fork();
 			if (pid == -1)
 				syscall_error();
-			if (pid == 0)
+			if (pid ==0 )
 			{
-				close(fds[0]);
 				dup2(fds[1],1);
 				close(fds[1]);
-				if (execve(cmd->opt[0],cmd->opt,envp) == -1)
-					execve_error(cmd->opt[0]);
+				close(fds[0]);
+				if (!strcmp(cmds->opt[0],"cd"))
+				{
+					ft_chdir(cmds);
+					exit(1);
+				}
+				if (execve(cmds->opt[0],cmds->opt,envp) == -1)
+					execve_error(cmds->opt[0]);
+				exit(1);
 			}
 			else
 			{
 				dup2(fds[0],0);
-				close(fds[0]);
 				close(fds[1]);
-			}
+				close(fds[0]);
+			}	
 		}
-		cmd = cmd->next;
+		cmds = cmds->next;
 	}
-	dup2(tmp_in,0);
-	while(waitpid(-1,NULL,0) > 0)
+	if (dup2(tmp_in,0) == -1)
+		syscall_error();
+	close(tmp_in);
+}
+void execute_one(commands *cmds, char **envp)
+{
+	if (!strcmp(cmds->opt[0],"cd"))
+	{
+		ft_chdir(cmds);
+		return ;
+	}
+	int pid;
+	pid = fork();
+	if (pid == -1)
+		syscall_error();
+	if (pid ==0 )
+	{
+		if (execve(cmds->opt[0],cmds->opt,envp) == -1)
+			execve_error(cmds->opt[0]);
+		exit(1);
+	}
+}
+
+void exec(commands *cmds, char **envp)
+{
+	if (cmds->next)
+		execute(cmds,envp);
+	else
+		execute_one(cmds,envp);
+	
+	while(waitpid(-1,NULL,0) != -1)
 		;
 }
 
-void execute(cmds *cmd,char **envp)
+void free_all(commands *cmds)
 {
-	if (!cmd->next)
+	commands *tmp;
+	while (cmds)
 	{
-		exec_single_cmd(cmd,envp);
-		return ;
+		tmp = cmds;
+		cmds = cmds->next;
+		free(tmp);
 	}
-	else
-		exec_cmds(cmd,envp);
+	
 }
 
-int main(int ac, char *av[], char *envp[])
+int main(int ac,char *av[],char *envp[])
 {
-	cmds *cmd;
+	commands *cmd = NULL;
 	if (ac < 2)
 		return 0;
 	av++;
-	while (*av)
+	while(av && *av)
 	{
 		av = parse(&cmd,av);
 		if (cmd)
-			execute(cmd,envp);
-		else
-			break;
+		{
+			exec(cmd,envp);
+			free_all(cmd);
+		}
 	}
+	return 0;
+	
 }
